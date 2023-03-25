17a168bf5bd0dca17fc069dd271a3e9d83f980ca
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2023-03-25 14:08:57 -0300

    feat(docker): Ahora si funciona el networking entre el container del backend y el container de la base de datos utilizando Docker compose.
    El problema era que dentro de la network que comparten ambos containers,
    cada uno es un host diferente, y se tiene que hacer referencia al host
    especifico (usando su hostname) para poder acceder a su red (en vez de
    `locahost:5432` seria `database:5432`, siendo `database` el nombre del
    servicio en el compose).

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,16 @@
 use std::net::SocketAddr;
 
 use axum::{routing::get, Router, Server};
+use sea_orm::Database;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(root));
     let addr: SocketAddr = "0.0.0.0:5000".parse().unwrap();
+
+    let _ = Database::connect("postgres://postgres:postgres@database:5432/postgres")
+        .await
+        .unwrap();
 
     Server::bind(&addr)
         .serve(app.into_make_service())
--- database/src/main.rs
+++ database/src/main.rs
@@ -4,6 +4,17 @@ use std::{fs::File, io::Write};
 async fn main() {
     let mut outfile = File::create("init.sql").unwrap();
     outfile
-        .write_all("CREATE TABLE IF NOT EXISTS materias ();".as_bytes())
+        .write_all(
+            "\
+CREATE TABLE IF NOT EXISTS materias (
+    codigo INTEGER PRIMARY KEY,
+    nombre TEXT NOT NULL
+);
+
+INSERT INTO materias (codigo, nombre) VALUES (6103, 'ANALISIS MATEMATICO II A');
+INSERT INTO materias (codigo, nombre) VALUES (6106, 'PROBABILIDAD Y ESTADISTICA A');
+"
+            .as_bytes(),
+        )
         .unwrap();
 }
