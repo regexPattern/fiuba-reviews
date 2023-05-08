8f6bb96d75fd4a6f6228cc636e0b2dbe82f53dd3
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2023-05-08 18:16:31 -0300

    fix: agregada la clausula de restart para el container del backend en el docker compose
    Esto soluciona el error de inicializacion del backend en la primera construccion de los containers, cuando se esta initializando la db.

--- database/src/materia.rs
+++ database/src/materia.rs
@@ -1,23 +1,30 @@
+use std::collections::HashMap;
+
 use format_serde_error::SerdeError;
 use reqwest_middleware::ClientWithMiddleware;
 use serde::Deserialize;
 use serde_with::{serde_as, DisplayFromStr};
 
-const URL_DESCARGA: &'static str =
+const URL_DESCARGA_MATERIAS: &'static str =
     "https://raw.githubusercontent.com/lugfi/dolly/master/data/comun.json";
+const URL_DESCARGA_EQUIVALENCIAS: &'static str = "https://raw.githubusercontent.com/lugfi/dolly/f47f553a89dc7c7cbf8192277c9f2e3e1e826bf0/data/equivalencias.json";
 
 pub const TABLA: &'static str = "\
 CREATE TABLE IF NOT EXISTS materias (
     codigo INTEGER PRIMARY KEY,
-    nombre TEXT NOT NULL
+    nombre TEXT NOT NULL,
+    codigo_equivalencia INTEGER
 );";
 
 #[serde_as]
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Materia {
     #[serde_as(as = "DisplayFromStr")]
     pub codigo: u32,
-    pub nombre: String,
+    nombre: String,
+
+    #[serde(skip)]
+    codigo_equivalencia: Option<u32>,
 }
 
 impl Materia {
@@ -29,18 +36,62 @@ impl Materia {
 
         tracing::info!("descargando listado de materias");
 
-        let respuesta = cliente.get(URL_DESCARGA).send().await?.text().await?;
-        let Respuesta { materias } =
+        let respuesta = cliente
+            .get(URL_DESCARGA_MATERIAS)
+            .send()
+            .await?
+            .text()
+            .await?;
+
+        let Respuesta { mut materias } =
             serde_json::from_str(&respuesta).map_err(|err| SerdeError::new(respuesta, err))?;
+
+        Self::asignas_equivalencias(cliente, &mut materias).await?;
 
         Ok(materias)
     }
 
+    async fn asignas_equivalencias(
+        cliente: &ClientWithMiddleware,
+        materias: &mut [Self],
+    ) -> anyhow::Result<()> {
+        tracing::info!("descargando listado de equivalencias");
+
+        let respuesta = cliente.get(URL_DESCARGA_EQUIVALENCIAS).send().await?;
+        let codigos_equivalencias: Vec<Vec<u32>> = respuesta.json().await?;
+
+        let mut equivalencias = HashMap::new();
+        for codigos in codigos_equivalencias {
+            let mut codigos = codigos.into_iter();
+
+            let codigo_materia_principal = match codigos.next() {
+                Some(codigo) => codigo,
+                None => continue,
+            };
+
+            for codigo in codigos {
+                equivalencias.insert(codigo, codigo_materia_principal);
+            }
+        }
+
+        tracing::info!("asignando equivalencias a materias");
+
+        for materia in materias {
+            materia.codigo_equivalencia = equivalencias.get(&materia.codigo).cloned();
+        }
+
+        Ok(())
+    }
+
     pub fn sql(&self) -> String {
-        format!(
-            "INSERT INTO materias (codigo, nombre) VALUES ({}, '{}');",
-            self.codigo,
-            self.nombre.replace("'", "''")
-        )
+        let mut columnas = "codigo, nombre".to_string();
+        let mut valores = format!("{}, '{}'", self.codigo, self.nombre.replace("'", "''"));
+
+        if let Some(codigo_equivalencia) = self.codigo_equivalencia {
+            columnas.push_str(", codigo_equivalencia");
+            valores.push_str(&format!(", {codigo_equivalencia}"));
+        }
+
+        format!("INSERT INTO materias ({columnas}) VALUES ({valores});")
     }
 }
