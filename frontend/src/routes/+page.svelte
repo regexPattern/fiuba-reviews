4486bc4518c3d8df47b1733d718708f80d783b1d
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2023-05-10 08:27:51 -0300

    fix: cambiado los endpoints antiguos del frontend

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -11,7 +11,9 @@
 	{#each data.materias as materia}
 		{#if materia.nombre.includes(searchQuery.toLowerCase()) || (searchQuery.length === 4 && materia.codigo === parseInt(searchQuery, 10))}
 			<li class="uppercase">
-				<a href={`/materias/${materia.codigo}`}>{materia.codigo} - {materia.nombre}</a>
+				<a href={`/materia/${materia.codigo_equivalencia || materia.codigo}`}
+					>{materia.codigo} - {materia.nombre}</a
+				>
 			</li>
 		{/if}
 	{/each}
