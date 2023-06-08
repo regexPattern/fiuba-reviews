cb0a19f3b420ec85ddf1769bf0eac83cbe59ad9c
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2023-06-08 20:52:43 -0300

    feat: prisma and homepage
    Added prisma client and finished first version of the homepage with
    example comments.

---
+++ frontend/src/lib/components/TarjetaComentarioEjemplo.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let contenido: string;
+	export let cuatrimestre: string;
+	export let docente: { nombre: string };
+</script>
+
+<article class="flex flex-col rounded-md border p-4">
+	<p class="line-clamp-6 flex-1">{contenido}</p>
+	<p class="mt-2 text-right">
+		{docente.nombre} - {cuatrimestre}
+	</p>
+</article>
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,12 +1,23 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config}*/
 const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ["./src/**/*.{html,js,svelte,ts}"],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				bgLight: colors.slate["50"],
+				bgDark: colors.slate["950"],
+			},
+			fontFamily: {
+				sans: ["Inter", ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 
-	plugins: []
+	plugins: [],
 };
 
 module.exports = config;
