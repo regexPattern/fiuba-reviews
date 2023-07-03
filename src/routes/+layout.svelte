0fc4ec30a0b112b24a4e839898a763aa10f42b7d
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2023-07-03 14:01:04 -0300

    estilos: agregado el ocultar a la sidebar en movil
    Tambien solucione un problema con el sticky de todo el aside cuando se
    hacia scroll desde el fondo del documento hacia arriba

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <header class="border-border bg-background sticky top-0 z-30 h-16 border-b bg-white" />
-<div class="container mx-auto flex flex-1">
+<div class="lg:container lg:mx-auto flex flex-1">
 	<slot />
 </div>
 
