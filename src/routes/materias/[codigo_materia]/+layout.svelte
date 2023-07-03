2bff83aee24d4ed7c547cbe8d4c766b2f298c785
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2023-07-03 14:01:04 -0300

    estilos: agregado el ocultar a la sidebar en movil
    Tambien solucione un problema con el sticky de todo el aside cuando se
    hacia scroll desde el fondo del documento hacia arriba

--- src/routes/materias/[codigo_materia]/+layout.svelte
+++ src/routes/materias/[codigo_materia]/+layout.svelte
@@ -2,24 +2,45 @@
 	import type { LayoutData } from "./$types";
 
 	export let data: LayoutData;
+
+	let mostrarCatedras = false;
+
+	function mostrarOOcultarCatedras() {
+		mostrarCatedras = !mostrarCatedras;
+	}
 </script>
 
-<div class="relative flex flex-1">
-	<aside class="sticky top-0 flex max-h-[calc(100vh-4rem)] w-80 shrink-0 flex-col overflow-y-auto">
-		<div class="sticky top-0 p-4 bg-white">{data.materia.codigo} - {data.materia.nombre}</div>
+<div class="flex flex-1 flex-col lg:flex-row">
+	<aside
+		class="top-16 flex shrink-0 flex-col overflow-y-auto lg:sticky lg:max-h-[calc(100vh-4rem)] lg:w-80"
+	>
+		<div class="top-0 border-b bg-white p-2 lg:sticky lg:border-b-0">
+			<button
+				class="w-full p-2 text-left"
+				on:click={mostrarOOcultarCatedras}
+				aria-controls="listado-catedras"
+				aria-expanded={mostrarCatedras}
+			>
+				{data.materia.codigo} - {data.materia.nombre}
+			</button>
+		</div>
 
-		<ul class="flex-1">
+		<ul
+			id="listado-catedras"
+			class={`flex-1 border-b p-2 lg:block lg:border-b-0 ${mostrarCatedras ? "block" : "hidden"}`}
+		>
 			{#each data.catedras as catedra}
-				<li class="p-2">
-					<a href={`/materias/${data.materia.codigo}/${catedra.codigo}`}
-						>{catedra.promedio.toFixed(1)} - {catedra.nombre}</a
+				<li class="flex items-center gap-1 p-2">
+					<span>{catedra.promedio.toFixed(1)}</span>
+					<a class="flex-1 pl-1" href={`/materias/${data.materia.codigo}/${catedra.codigo}`}
+						>{catedra.nombre}</a
 					>
 				</li>
 			{/each}
 		</ul>
 	</aside>
 
-	<main class="flex-1">
+	<main class="flex-1 p-4">
 		<slot />
 	</main>
 </div>
