ee963e3e871aeae3d3f4d7400329f2cd113bd27b
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2023-11-15 19:08:02 -0300

    wip: Corregido el error de layout zooming en iOS y tambien implementado el funcionamiento general de la barra de busqueda.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -37,7 +37,7 @@
 			m.nombre.includes(search.toUpperCase())
 	);
 
-	$: if (search.length === 0) {
+	$: if (search.length === 0 || open == false) {
 		filtered = [];
 	}
 
@@ -49,13 +49,14 @@
 			}
 		}
 		document.addEventListener("keydown", handleKeydown);
+
 		return () => {
 			document.removeEventListener("keydown", handleKeydown);
 		};
 	});
 </script>
 
-<header class="sticky top-0 z-20 border-b bg-background/70 p-4 backdrop-blur-lg">
+<header class="sticky top-0 z-20 space-x-2 border-b bg-background/70 p-3 backdrop-blur-lg">
 	<Button
 		variant="outline"
 		on:click={() => (open = !open)}
@@ -66,6 +67,7 @@
 			<span>⌘</span> K
 		</kbd>
 	</Button>
+
 	<DropdownMenu positioning={{ placement: "bottom-end" }}>
 		<DropdownMenuTrigger asChild let:builder>
 			<Button builders={[builder]} variant="outline" size="icon">T</Button>
@@ -82,10 +84,11 @@
 	<CommandInput placeholder="Código o nombre de una materia" on:input={debounceSearch} />
 	<CommandList>
 		{#each filtered as mat}
+			{@const slug = mat.codigoEquivalencia || mat.codigo}
 			<CommandItem
 				value={mat.codigo}
 				onSelect={async () => {
-					await goto(`/materias/${mat.codigo}`);
+					await goto(`/materias/${slug}`);
 					open = false;
 				}}
 			>
