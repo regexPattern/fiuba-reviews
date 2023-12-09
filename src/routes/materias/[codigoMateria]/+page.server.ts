be9eae4175b3c7466e80e5bc446de69c9e2af578
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2023-12-09 00:33:29 -0300

    fix: Solucionado el problema de las materias equivalentes

--- src/routes/materias/[codigoMateria]/+page.server.ts
+++ src/routes/materias/[codigoMateria]/+page.server.ts
@@ -7,38 +7,11 @@ import type { PageServerLoad } from "./$types";
 
 export const prerender = true;
 
-export const load: PageServerLoad = async ({ params }) => {
-  const codigoMateria = parseInt(params.codigoMateria, 10);
+export const load: PageServerLoad = async ({ parent }) => {
+  const layoutData = await parent();
 
-  const defaultCatedra = (
-    await db
-      .select({
-        codigo: catedra.codigo,
-        promedio: sql<number>`
-AVG((
-  SELECT AVG((
-    ${calificacion.aceptaCritica} 
-      + ${calificacion.asistencia} 
-      + ${calificacion.buenTrato} 
-      + ${calificacion.claridad} 
-      + ${calificacion.claseOrganizada} 
-      + ${calificacion.cumpleHorarios} 
-      + ${calificacion.fomentaParticipacion} 
-      + ${calificacion.panoramaAmplio} 
-      + ${calificacion.respondeMails}) / 9)
-	FROM ${calificacion}
-	WHERE ${calificacion.codigoDocente} = ${docente.codigo}
-  GROUP BY ${docente.codigo})
-)`
-      })
-      .from(catedra)
-      .innerJoin(catedraDocente, eq(catedra.codigo, catedraDocente.codigoCatedra))
-      .innerJoin(docente, eq(docente.codigo, catedraDocente.codigoDocente))
-      .where(eq(catedra.codigoMateria, codigoMateria))
-      .groupBy(catedra.codigo)
-      .orderBy(({ promedio }) => desc(promedio))
-      .limit(1)
-  )[0];
+  const codigoMateria = layoutData.materia.codigo;
+  const defaultCatedra = layoutData.catedras[0];
 
   throw redirect(307, `/materias/${codigoMateria}/${defaultCatedra.codigo}`);
 };
