ffdad57a1b13440a78a4609fd7acd1e9290f840a
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2024-06-29 21:00:24 -0300

    fix: cambiado la visibilidad de las api keys de posthog a public

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,14 +1,16 @@
 import { browser, dev } from "$app/environment";
-import { env } from "$env/dynamic/private";
+import { env } from "$env/dynamic/public";
 import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
 import posthog from "posthog-js";
 
 export const load = async () => {
 	if (!dev && browser) {
-		posthog.init(env.POSTHOG_PROJECT_API_KEY, {
-			api_host: "https://us.i.posthog.com",
-			person_profiles: "never"
-		});
+		if (env.PUBLIC_POSTHOG_PROJECT_API_KEY) {
+			posthog.init(env.PUBLIC_POSTHOG_PROJECT_API_KEY, {
+				api_host: "https://us.i.posthog.com",
+				person_profiles: "never"
+			});
+		}
 		injectSpeedInsights();
 	}
 };
