7d20ac21242876f3278c4f7c6bb763cb3da7590d
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2025-02-04 19:22:00 -0300

    Scraper y Vercel Functions (#14)
    * Scraper SIU (#12)
    
    Implementación del paquete que obtiene la información de las materias, cátedras y docentes del SIU Guaraní para generar el listado de los mismos que va a usar FIUBA Reviews.
    
    * Scraper SIU API (#13)
    
    * git: reorganizado el repo para el scraper y el backend
    
    * feat: agregado el endpoint POST
    
    Todavía no hay soporte para distinguir si necesito la información
    enviada o no
    
    * feat: mejorada la funcionalidad de metadatos del siu
    
    * git: ajustada la estructura de los directorios
    
    Ahora se va a hostear el backend como serverless functions en Vercel.
    
    * ci: configurados dos directorios diferentes para proyectos en Vercel

---
+++ app/svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from "@sveltejs/adapter-vercel";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+/** @type {import("@sveltejs/kit").Config} */
+const config = {
+  preprocess: [vitePreprocess({})],
+  kit: {
+    adapter: adapter({
+      runtime: "nodejs18.x",
+    }),
+    prerender: {
+      handleHttpError: "fail",
+    },
+  },
+};
+
+export default config;
---
+++ resumidor/src/main.rs
@@ -0,0 +1,64 @@
+use std::{env, io::Write};
+
+use clap::{Parser, Subcommand};
+use resumidor::llm::OllamaClient;
+use reqwest::Client;
+use sqlx::postgres::PgPoolOptions;
+
+const DATABASE_URL_ENV: &str = "DATABASE_URL";
+const OPENAI_API_KEY_ENV: &str = "OPENAI_API_KEY";
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    comand: Subcomando,
+}
+
+#[derive(Subcommand)]
+enum Subcomando {
+    Resumir,
+    Sanitizar,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    tracing_subscriber::fmt::init();
+    dotenvy::dotenv()?;
+
+    let db_url = env::var(DATABASE_URL_ENV).expect(const_format::formatcp!(
+        "variable de entorno `{}` necesaria para conectar con la base de datos",
+        DATABASE_URL_ENV
+    ));
+
+    let db = PgPoolOptions::new().connect(&db_url).await?;
+
+    tracing::info!("conexion establecida con la base de datos");
+
+    let _api_key = env::var(OPENAI_API_KEY_ENV).expect(const_format::formatcp!(
+        "variable de entorno `{}` necesaria para conectar con OpenAI API",
+        OPENAI_API_KEY_ENV
+    ));
+
+    let llm = OllamaClient {
+        //api_key,
+        http_client: Client::new(),
+    };
+
+    match cli.comand {
+        Subcomando::Resumir => {
+            if let Some(query) = resumidor::query_actualizacion_resumenes(llm, &db).await? {
+                let mut file = std::fs::File::create("update.sql")?;
+                file.write_all(query.as_bytes())?;
+
+                tracing::info!("query guardada en archivo `update.sql`");
+            } else {
+                tracing::info!("ningún docente se ha actualizado");
+            }
+        }
+        Subcomando::Sanitizar => {}
+    };
+
+    Ok(())
+}
