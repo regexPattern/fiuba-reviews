c8c41f9d4a75012056f9f62cb402016715a0b870
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2025-12-02 17:04:44 -0300

    git: reoganizando las apps principales del repo y eliminando version actual del actualizador

--- actualizador-ofertas/.docker/postgres/custom-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-curl -L \
-  -o /docker-entrypoint-initdb.d/public-schema.sql.gz \
-  https://github.com/user-attachments/files/19075454/public-schema.sql.gz
-
-exec /usr/local/bin/docker-entrypoint.sh "$@"
